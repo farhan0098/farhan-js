dc5d274fcefb79bd27e687c9a8df38e29e839d66
Farhan Ali <159815493+farhan0098@users.noreply.github.com>
2024-03-03 21:31:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// var num =2024
+// var num = num++
+// alert(num)
+// var userInput = +prompt( "enter your age");
+// alert(userInput +2 );
+//1) 2*2+4-2/2;
+//2)  3+2*4;
+// 3) (2+3)*7;
+// 4) 4*(5+7);
+// 5) 6/2+3;
+// bodmas
+var calculation =2*4+3;
+alert(calculation)
+var calculation =(2+3)*7;
+alert(calculation)
+var calculation =(5+7)*4;
+alert(calculation)
+var calculation =6/2+3;
+alert(calculation)
